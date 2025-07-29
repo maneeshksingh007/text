e6688a1dc5becb80106f0898fe168bd68a5fd190
alok9368 <alok.rajput@gmail.com>
2025-07-29 08:47:17 +0530

    added

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+---------------
+
+resource "azurerm_resource_group" "rgs" {
+  name     = "alok"
+  location = "West Europe"
+}
